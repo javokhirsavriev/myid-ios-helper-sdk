12a048198f72cb87c579992a982b43286471b618
Javokhir <javokhirdev@gmail.com>
2025-02-18 12:52:01 +0500

    1.0.0

---
+++ MyIdHelperSDK.xcframework/ios-arm64/MyIdHelperSDK.framework/Headers/MyIdHelperSDK.h
@@ -0,0 +1,18 @@
+//
+//  MyIdHelperSDK.h
+//  MyIdHelperSDK
+//
+//  Created by Javohir Savriy on 18/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyIdHelperSDK.
+FOUNDATION_EXPORT double MyIdHelperSDKVersionNumber;
+
+//! Project version string for MyIdHelperSDK.
+FOUNDATION_EXPORT const unsigned char MyIdHelperSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyIdHelperSDK/PublicHeader.h>
+
+
